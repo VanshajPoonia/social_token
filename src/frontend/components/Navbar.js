2c0a4f2ca9f0781da3adcac14193edaa1eda921c
Vanshaj Poonia <74147463+VanshajPoonia@users.noreply.github.com>
2022-12-03 10:41:47 +0530

    Add files via upload

---
+++ src/frontend/components/Navbar.js
@@ -0,0 +1,46 @@
+import {
+    Link
+} from "react-router-dom";
+import { Navbar, Nav, Button, Container } from 'react-bootstrap'
+import market from './market.png'
+
+const Navigation = ({ web3Handler, account }) => {
+    return (
+        <Navbar expand="lg" bg="secondary" variant="dark">
+            <Container>
+                <Navbar.Brand href="http://localhost:3000">
+                    <img src={market} width="40" height="40" className="" alt="" />
+                    &nbsp; Social Token
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/create">Ask</Nav.Link>
+                        <Nav.Link as={Link} to="/my-listed-items">My Aid</Nav.Link>
+                        <Nav.Link as={Link} to="/my-purchases">My Achievements</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        {account ? (
+                            <Nav.Link
+                                href={`https://etherscan.io/address/${account}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="button nav-button btn-sm mx-4">
+                                <Button variant="outline-light">
+                                    {account.slice(0, 5) + '...' + account.slice(38, 42)}
+                                </Button>
+
+                            </Nav.Link>
+                        ) : (
+                            <Button onClick={web3Handler} variant="outline-light">Connect Wallet</Button>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+
+}
+
+export default Navigation;
